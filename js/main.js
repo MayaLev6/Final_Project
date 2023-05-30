47461deffb26d15dd870a5dbf9db3b74b7fe5c39
Maya Lev <maya.lev.2009@gmail.com>
2023-05-30 19:22:18 +0300

    media update

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ window.addEventListener("scroll", function(){
 
     if(window.pageYOffset > 650){
         logoImage.style.height = "60px";
-        logoImage.src = "media/Logo.png";
+        logoImage.src = "media/homepageMedia/Logo.png";
         mainNav.classList.add('bg-black');
         mainNav.classList.add("txt-white");
     }
     else{
         logoImage.style.height = "80px";
-        logoImage.src = "media/Logo-Black.png";
+        logoImage.src = "media/homepageMedia/Logo-Black.png";
         mainNav.classList.remove('bg-black');
         mainNav.classList.remove("txt-white");
     }
